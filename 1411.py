42344ca55c3e211e9048a3d7433b04c2569f2798
Julianne <U_sub@mail.ru>
2022-11-12 00:03:48 +0300

    a

---
+++ 1411.py
@@ -0,0 +1,14 @@
+n = int(input())
+k =0
+a = list(map(int,input().split()))
+for i in range (len(a)):
+    f = False
+    for j in range (len(a)-1, 0, -1):
+
+        if a[j] < a [j-1]:
+            a [j], a [j-1] = a[j-1], a[j]
+            f = True
+            k+=1
+    if not f:
+            break
+print(k)
