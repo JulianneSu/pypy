12cd1ce84113f501c0bcc0ee565888f5a940715f
Julianne <U_sub@mail.ru>
2022-11-11 23:17:38 +0300

    a

--- 230.py
+++ 230.py
@@ -3,10 +3,10 @@ a = list(map(int,input().split()))
 
 for i in range (len(a)-1):
     ind = i
-    for j in range(i, len(a)):
-        if a[i] < a[j]:
+    for j in range(i+1, len(a)):
+        if a[ind] < a[j]:
             ind = j
-        a[i], a[ind] = a[ind], a[i]
+    a[i], a[ind] = a[ind], a[i]
 
 
 
