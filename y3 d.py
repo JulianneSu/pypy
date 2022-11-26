3da66a51c5a537e4ec5d76e104738c432889ed96
JulianneSu <U_sub@mail.ru>
2022-11-26 14:24:21 +0300

    2

---
+++ y3 d.py
@@ -0,0 +1,14 @@
+n = int(input())
+m = int(input())
+a = set()
+b = set()
+for i in range(n):
+    q = input()
+    a.add(q)
+for i in range(m):
+    q = input()
+    b.add(q)
+if a & b == set():
+    print('Таких нет')
+else:
+    print(len(a & b))
